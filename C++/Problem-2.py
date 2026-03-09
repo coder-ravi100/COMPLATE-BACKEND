57a8ede3b46bd352dc2360e3f14aaf7abb9b39f3
coder-ravi100 <ravirathod100100@gmail.com>
2026-03-09 19:15:51 +0530

    update concepts file

--- C++/Problem-2.py
+++ C++/Problem-2.py
@@ -1,34 +1,35 @@
 #Problem 2: Student System
 class Student:
-    def __init__(self,name, marks1,marks2,marks3):
+
+    def __init__(self, name, marks1, marks2, marks3):
         self.name = name
         self.marks1 = marks1
         self.marks2 = marks2
         self.marks3 = marks3
 
     def total_marks(self):
-        self.student_marks = self.marks1 + self.marks2 + self.marks3
-        
-    def avg(self):
-        self.student_avg = self.student_marks / 3
+        self.total = self.marks1 + self.marks2 + self.marks3
+
+    def average(self):
+        self.avg = self.total / 3
 
     def result(self):
-        if self.student_marks >= 80:
-            self.result = "Pass"
+        if self.avg >= 40:
+            self.result_status = "Pass"
         else:
-            self.result = "Fail"
-
+            self.result_status = "Fail"
 
     def display(self):
-        print("Student Name :",self.name)
-        print("Student Total Marks :",self.student_marks)
-        print("Student Average :",self.student_avg)
-        print("Student Result :",self.result)
+        print("Student Name:", self.name)
+        print("Total Marks:", self.total)
+        print("Average:", self.avg)
+        print("Result:", self.result_status)
 
-        
-s1 = Student("Ravi",50,60,50)
+
+s1 = Student("Ravi", 50, 60, 50)
+
 s1.total_marks()
-s1.avg()
+s1.average()
 s1.result()
 s1.display()
 
